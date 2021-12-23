4c4e60e3d029ba42eb2527c5703befc363761e14
silveiraSoft <adalbertosn1982@gmail.com>
2021-12-23 12:26:29 -0600

    first commit

---
+++ src/main/java/com/adalbertosn/demo1_springboot/Demo2SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.adalbertosn.demo1_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2SpringbootApplication.class, args);
+		System.out.println("ok");
+	}
+
+}
---
+++ src/test/java/com/adalbertosn/demo1_springboot/Demo2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn.demo1_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
