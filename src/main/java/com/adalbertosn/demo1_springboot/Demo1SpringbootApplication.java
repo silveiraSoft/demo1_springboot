605621174a03359fbdd5fde27429e4d8b3e40ca9
silveiraSoft <adalbertosn1982@gmail.com>
2021-12-23 12:32:06 -0600

    chage the name of project class

--- src/main/java/com/adalbertosn/demo1_springboot/Demo1SpringbootApplication.java
+++ src/main/java/com/adalbertosn/demo1_springboot/Demo1SpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo2SpringbootApplication {
+public class Demo1SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2SpringbootApplication.class, args);
+		SpringApplication.run(Demo1SpringbootApplication.class, args);
 		System.out.println("ok");
 	}
 
--- src/test/java/com/adalbertosn/demo1_springboot/Demo1SpringbootApplicationTests.java
+++ src/test/java/com/adalbertosn/demo1_springboot/Demo1SpringbootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo2SpringbootApplicationTests {
+class Demo1SpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
